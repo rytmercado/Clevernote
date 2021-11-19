1076166b23cf49ff25c77dc04fce8794637309ae
Ry Mercado <rytmercado@gmail.com>
2021-11-19 16:42:27 -0700

    minor cleanup

--- frontend/clevernote.jsx
+++ frontend/clevernote.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import configureStore from './store/store'
-import Root from './components/root'
-import {signup, login, logout} from './actions/session_actions'
+import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     const store = configureStore();
 
     ReactDOM.render(<Root store={store} />, root);
-    // window.receiveCurrentUser = receiveCurrentUser;
-    window.store = store;
-    window.signup = signup;
-    window.login = login;
-    window.logout = logout;
 })
