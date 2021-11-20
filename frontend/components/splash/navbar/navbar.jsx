028355218e98e4d54f7521b073fb4d061fc0da23
Ry Mercado <rytmercado@gmail.com>
2021-11-20 11:00:24 -0700

    sticky nav bar styling

--- frontend/components/splash/navbar/navbar.jsx
+++ frontend/components/splash/navbar/navbar.jsx
@@ -2,17 +2,14 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <header className='nav-container'>
-            <div className='row'>
-                <div className='navbar'>
-                    <div className='left-nav'>
-                        <a>Clevernote</a>
-                        <a>Github</a>
-                    </div>
-                    <div className='right-nav'>
-                        <a>Login</a>
-                        <a>Sign Up!</a>
-                    </div>
+        <header className="nav-container">
+            <div className="topnav">
+                {/* <a>Home</a> */}
+                <a>Clevernote</a>
+                <a>Github</a>
+                <div className="topnav-right">
+                    <a>Help</a>
+                    <a>Login</a>
                 </div>
             </div>
         </header>
