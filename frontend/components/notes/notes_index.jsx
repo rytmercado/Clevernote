4bed45e831f5133f6c53fc2838651ed26d051260
Ry Mercado <rytmercado@gmail.com>
2022-02-18 12:58:02 -0700

    stop messing with css its fine

--- frontend/components/notes/notes_index.jsx
+++ frontend/components/notes/notes_index.jsx
@@ -42,37 +42,34 @@ export default class NotesIndex extends React.Component {
     render() {
         return(
             <div className="note-index">
-                <div className="notes-index">
-                    <header className="notes-header">
-                        <div className='notes-header-top'>
-                            <FontAwesomeIcon id="note-fai-large" icon={faNoteSticky} />
-                            <h1 id="notes-header-fix">Notes</h1>
-                        </div>
-                        <a id='note-count'>{this.props.notes.length + ' notes'}</a>
-                    </header>
-                    <div>
-
-                        <ul>
-                            {this.props.notes.map(note => {
-                                let url = `/notes/${note.id}`
-                                return(
-                                    <div className={`note-index-container${this.state.activeWord === note.id ? ' selected-note' : ''}`} onClick={() => this.selectedNote(note.id)} key={note.id} >
-                                        <Link to={url} >
-                                            <li  className="note-index-item" >{note.title}</li>
-                                            <div className="delete-note-container">
-
-                                                <FontAwesomeIcon onClick={() => this.props.deleteNote(note.id)} icon={faTrash} />
-                                            </div>
-                                            <div className="note-index-item-bottom">
-                                                <span id='note-count'>{'last updated ' + timeSince(note.updated_at) + ' ago'}</span>
-                                            </div>
-                                        </Link>
-                                    </div>
-                                )
-                                })
-                            }
-                        </ul>
+                <header className="notes-header">
+                    <div className='notes-header-top'>
+                        <FontAwesomeIcon id="note-fai-large" icon={faNoteSticky} />
+                        <h1 id="notes-header-fix">Notes</h1>
                     </div>
+                    <a id='note-count'>{this.props.notes.length + ' notes'}</a>
+                </header>
+                <div className="note-index-scroll">
+                    <ul >
+                        {this.props.notes.map(note => {
+                            let url = `/notes/${note.id}`
+                            return(
+                                <div className={`note-index-container${this.state.activeWord === note.id ? ' selected-note' : ''}`} onClick={() => this.selectedNote(note.id)} key={note.id} >
+                                    <Link to={url} >
+                                        <li  className="note-index-item" >{note.title}</li>
+                                        <div className="delete-note-container">
+
+                                            <FontAwesomeIcon onClick={() => this.props.deleteNote(note.id)} icon={faTrash} />
+                                        </div>
+                                        <div className="note-index-item-bottom">
+                                            <span id='note-count'>{'last updated ' + timeSince(note.updated_at) + ' ago'}</span>
+                                        </div>
+                                    </Link>
+                                </div>
+                            )
+                            })
+                        }
+                    </ul>
                 </div>
             </div>
             
