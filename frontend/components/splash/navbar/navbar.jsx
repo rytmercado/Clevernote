290832fef029c04ffd713b6b9082960058df4e38
Ry Mercado <rytmercado@gmail.com>
2022-03-29 13:20:32 -0600

    added socials to splash

--- frontend/components/splash/navbar/navbar.jsx
+++ frontend/components/splash/navbar/navbar.jsx
@@ -8,6 +8,8 @@ const Navbar = () => {
                 <img className="nav-logo" src={window.logo}></img>
                 <a href='#/' id="nav-title">Clevernote</a>
                 <a href='https://github.com/rytmercado' target="_blank">Github</a>
+                <a href='https://angel.co/ry-mercado' target="_blank">AngelList</a>
+                <a href='https://linkedin.com/in/ry-mercado' target="_blank">Linkedin</a>
                 <div className="topnav-right">
                     <a>Help</a>
                     <Link to="/login">
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -30,9 +30,20 @@ export default class Splash extends React.Component {
                                 Already have an account? Log in
                             </Link>
                         </div>
-                        <div>
+                        <div className='flex-simple'>
                             <img className="splash-image" src={window.splash}></img>
+                            <div className="splash-features">
+                                <h1>WORK ANYWHERE</h1>
+                                <p>Keep important info handy—your notes sync automatically to all your devices.</p>
+                                <h1>REMEMBER EVERYTHING</h1>
+                                <p>Make notes more useful by adding text, images, audio, scans, PDFs, and documents.</p>
+                                <h1>TURN TO-DO INTO DONE</h1>
+                                <p>Bring your notes, tasks, and schedules together to get things done more easily.</p>
+                                <h1>FIND THINGS FAST</h1>
+                                <p>Get what you need, when you need it with powerful, flexible search capabilities.</p>
+                            </div>
                         </div>
+                        <h1 className='splash-slogan-header-small'>Find your productivity happy place</h1>
                     </div>
                 </div>
             </div>
